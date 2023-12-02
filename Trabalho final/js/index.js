8d1dce3f3a400591f539eef904c448349ce78ebb
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-12-02 16:17:28 -0300

    add js do trabalho final de desv web

--- Trabalho final/js/index.js
+++ Trabalho final/js/index.js
@@ -1,47 +1,85 @@
-const Elemento_do_tabuleiro = document.getElementById('quadro');
-const Jogador1 = { dados: Array(9).fill(0) };
-const Jogador2 = { dados: Array(9).fill(0) };
-let Jogador_atual = Jogador1;
-
-function renderizar() {
-    Elemento_do_tabuleiro.innerHTML = '';
-  for (let i = 0; i < 9; i++) {
-    const celula = document.createElement('div');
-    celula.classList.add('celula');
-    celula.textContent = Jogador_atual.dados[i];
-    celula.onclick = () => colocarDados(i);
-    Elemento_do_tabuleiro.appendChild(celula);
-  }
-}
-//valor_dos_dados
-
-function lancar_dados() {
-  const valor_dos_dados = Math.floor(Math.random() * 6) + 1;
-  Jogador_atual.dados = Array(9).fill(valor_dos_dados);
-  renderizar();
+// Função para criar um tabuleiro vazio de 3x3
+function criarTabuleiroVazio() {
+  return Array.from({ length: 3 }, () => Array(3).fill(null));
 }
 
-function colocarDados(column) {
-    Jogador_atual.dados[column]++;
+// Função para renderizar o tabuleiro na interface
+function renderizarTabuleiro(tabuleiro, jogadorId) {
+  // Obtém o elemento HTML do tabuleiro do jogador
+  const tabuleiroElemento = document.getElementById(`jogador${jogadorId}-tabuleiro`);
+  // Limpa o conteúdo do elemento
+  tabuleiroElemento.innerHTML = '';
 
-  const adversario = Jogador_atual === Jogador1 ? Jogador2 : Jogador1;
-  adversario.dados = adversario.dados.map((value, index) => (index % 3 === column % 3 ? 0 : value));
+  // Loop para percorrer as linhas do tabuleiro
+  for (let i = 0; i < tabuleiro.length; i++) {
+    // Cria um elemento de linha para cada linha do tabuleiro
+    const linhaElemento = document.createElement('div');
+    linhaElemento.className = 'linha';
 
-  renderizar();
+    // Loop para percorrer as colunas do tabuleiro
+    for (let j = 0; j < tabuleiro[i].length; j++) {
+      // Cria um elemento de célula para cada célula do tabuleiro
+      const celulaElemento = document.createElement('div');
+      celulaElemento.className = 'celula';
+      // Define o texto da célula com o valor do tabuleiro ou uma string vazia se for nulo
+      celulaElemento.innerText = tabuleiro[i][j] || '';
+      // Adiciona um evento de clique para lidar com a interação do jogador
+      celulaElemento.onclick = () => lidarClique(jogadorId, i, j);
+      // Adiciona a célula à linha
+      linhaElemento.appendChild(celulaElemento);
+    }
 
-  if (Jogador_atual.dados.every(value => value > 0)) {
-    alert(`Jogador ${Jogador_atual === Jogador1 ? 1 : 2} ganhou!`);
-    reinciar();
-  } else {
-    Jogador_atual = Jogador_atual === Jogador1 ? Jogador2 : Jogador1;
+    // Adiciona a linha ao tabuleiro
+    tabuleiroElemento.appendChild(linhaElemento);
   }
 }
 
-function reinciar() {
-    Jogador1.dados = Array(9).fill(0);
-    Jogador2.dados = Array(9).fill(0);
-    Jogador_atual = Jogador1;
-    renderizar();
+// Função para lidar com o clique do jogador em uma célula
+function lidarClique(jogadorId, linha, coluna) {
+  // Determina o tabuleiro atual e o tabuleiro do oponente com base no jogador
+  const tabuleiroAtual = jogadorId === 1 ? jogador1Tabuleiro : jogador2Tabuleiro;
+  const tabuleiroOponente = jogadorId === 1 ? jogador2Tabuleiro : jogador1Tabuleiro;
+
+  // Gera um número aleatório de 1 a 6 e preenche a célula clicada
+  if (!tabuleiroAtual[linha][coluna]) {
+    tabuleiroAtual[linha][coluna] = Math.floor(Math.random() * 6) + 1;
+  }
+
+  // Se o valor na célula atual for igual ao do oponente, zera a linha correspondente no tabuleiro do oponente
+  if (tabuleiroAtual[linha][coluna] === tabuleiroOponente[linha][coluna]) {
+    tabuleiroOponente[linha].fill(0);
+  }
+
+  // Atualiza a renderização dos tabuleiros na interface
+  renderizarTabuleiro(jogador1Tabuleiro, 1);
+  renderizarTabuleiro(jogador2Tabuleiro, 2);
+
+  // Verifica se o jogo chegou ao fim
+  if (tabuleiroAtual.every(linha => linha.every(celula => celula !== null))) {
+    // Calcula a pontuação final de cada jogador
+    const pontuacaoJogador1 = tabuleiroAtual.flat().reduce((acc, valor) => acc + (valor || 0), 0);
+    const pontuacaoJogador2 = tabuleiroOponente.flat().reduce((acc, valor) => acc + (valor || 0), 0);
+
+    // Exibe uma mensagem de pontuação final
+    alert(`Pontuação Final: \nJogador 1: ${pontuacaoJogador1}\nJogador 2: ${pontuacaoJogador2}`);
+
+    // Reinicia o jogo
+    reiniciarJogo();
+  }
 }
 
-renderizar();
+// Função para reiniciar o jogo
+function reiniciarJogo() {
+  // Cria tabuleiros vazios para ambos os jogadores
+  jogador1Tabuleiro = criarTabuleiroVazio();
+  jogador2Tabuleiro = criarTabuleiroVazio();
+  // Atualiza a renderização dos tabuleiros na interface
+  renderizarTabuleiro(jogador1Tabuleiro, 1);
+  renderizarTabuleiro(jogador2Tabuleiro, 2);
+}
+
+// Inicialização dos tabuleiros e renderização inicial na interface
+let jogador1Tabuleiro = criarTabuleiroVazio();
+let jogador2Tabuleiro = criarTabuleiroVazio();
+renderizarTabuleiro(jogador1Tabuleiro, 1);
+renderizarTabuleiro(jogador2Tabuleiro, 2);
